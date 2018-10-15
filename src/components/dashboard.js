0067e04912870e9f99bf475ba2a9a213ef08e274
Timothy Dao <timster78811@gmail.com>
2018-10-15 17:20:01 -0400

    added auth

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -14,7 +14,7 @@ export class Dashboard extends React.Component {
         <div className="dashboard-username">
           Username: {this.props.username}
         </div>
-        <div className="dashboard-name">Name: {this.props.name}</div>
+        {/* <div className="dashboard-name">Name: {this.props.name}</div> */}
         <div className="dashboard-protected-data">
           Protected data: {this.props.protectedData}
         </div>
@@ -27,7 +27,6 @@ const mapStateToProps = state => {
   const { currentUser } = state.auth;
   return {
     username: state.auth.currentUser.username,
-    name: `${currentUser.firstName} ${currentUser.lastName}`,
     protectedData: state.protectedData.data
   };
 };
