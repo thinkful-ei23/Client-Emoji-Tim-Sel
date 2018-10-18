deb14a63a3217247ae46d7d530b91c391fdd968f
Selvin Norman <selvin715@gmail.com>
2018-10-18 08:59:47 -0700

    css cleanup

--- src/components/emoji-card.js
+++ src/components/emoji-card.js
@@ -4,7 +4,6 @@ import emoji from 'emoji-name-map';
 import styles from './styles/card.module.css';
 
 export default function Card(props) {
-  console.log('props: ', props);
   const userAnsweredTag = <span className="correctAnswer">{props.description}</span>;
   let divResults;
   if (props.outcome === 'Incorrect') {
