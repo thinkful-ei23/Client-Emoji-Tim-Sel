a5e083ec4cb02e55af8318f9aa771542dce22477
selvinor <40412939+selvinor@users.noreply.github.com>
2018-11-26 13:15:48 -0800

    Update landing-page.js
    Changed 'course' to 'app' in description.

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -13,7 +13,7 @@
 //   return (
 //     <div className="home">
 //       <h2>Welcome to the Emoji Spaced Repetition App TEST !</h2>
-//       <p>This course will help you to communicate quickly and effortlessly, even across language barriers!</p>
+//       <p>This app will help you to communicate quickly and effortlessly, even across language barriers!</p>
 //       <p>Emojis are not only fun, they are fantastic time savers, increase social power, and are a valuable communication method!</p>
 //       <LoginForm />
 //       <Link to="/register">Register</Link>
